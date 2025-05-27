bbe8be8939d6f2bbc9f64646dbb1f3915feec199
el alami otmane‬‏ <alamiotman172@gmail.com>
2025-05-27 23:54:40 +0200

    Update Navbar.jsx

--- front end projet hopy/src/components/Navbar.jsx
+++ front end projet hopy/src/components/Navbar.jsx
@@ -26,88 +26,90 @@ const Navbar = () => {
   };
 
   return (
-    <header>
-      <nav className={iconActive ? "nav-active" : ""}>
-        <h2 className="nav-logo">
-          <NavLink to={"/"}>Maroc Hospital</NavLink>
-        </h2>
-        <ul className="nav-links">
-          <li>
-            <NavLink to={"/"}>Home</NavLink>
-          </li>
-          <li>
-            <NavLink to={"/doctors"}>Doctors</NavLink>
-          </li>
-          {token && user.isAdmin && (
-            <>
-              <li>
-                <NavLink to={"/dashboard/users"}>Dashboard</NavLink>
-              </li>
-              <li>
-                <NavLink to={"/dashboard/contact-messages"}>Messages</NavLink>
-              </li>
-            </>
-          )}
-          {token && !user.isAdmin && (
-            <>
-              <li>
-                <NavLink to={"/appointments"}>Appointments</NavLink>
-              </li>
-              <li>
-                <NavLink to={"/notifications"}>Notifications</NavLink>
-              </li>
-              <li>
-                <NavLink to={"/applyfordoctor"}>Apply for doctor</NavLink>
-              </li>
-              <li>
-                <HashLink to={"/#contact"}>Contact Us</HashLink>
-              </li>
-              <li>
-                <NavLink to={"/profile"}>Profile</NavLink>
-              </li>
-            </>
-          )}
-          {!token ? (
-            <>
-              <li>
-                <NavLink className="btn" to={"/login"}>
-                  Login
-                </NavLink>
-              </li>
-              <li>
-                <NavLink className="btn" to={"/register"}>
-                  Register
-                </NavLink>
-              </li>
-            </>
-          ) : (
+    <div className="nav-container">
+      <header>
+        <nav className={iconActive ? "nav-active" : ""}>
+          <h2 className="nav-logo">
+            <NavLink to={"/"}>Maroc Hospital</NavLink>
+          </h2>
+          <ul className="nav-links">
             <li>
-              <span className="btn" onClick={logoutFunc}>
-                Logout
-              </span>
+              <NavLink to={"/"}>Home</NavLink>
             </li>
+            <li>
+              <NavLink to={"/doctors"}>Doctors</NavLink>
+            </li>
+            {token && user.isAdmin && (
+              <>
+                <li>
+                  <NavLink to={"/dashboard/users"}>Dashboard</NavLink>
+                </li>
+                <li>
+                  <NavLink to={"/dashboard/contact-messages"}>Messages</NavLink>
+                </li>
+              </>
+            )}
+            {token && !user.isAdmin && (
+              <>
+                <li>
+                  <NavLink to={"/appointments"}>Appointments</NavLink>
+                </li>
+                <li>
+                  <NavLink to={"/notifications"}>Notifications</NavLink>
+                </li>
+                <li>
+                  <NavLink to={"/applyfordoctor"}>Apply for doctor</NavLink>
+                </li>
+                <li>
+                  <HashLink to={"/#contact"}>Contact Us</HashLink>
+                </li>
+                <li>
+                  <NavLink to={"/profile"}>Profile</NavLink>
+                </li>
+              </>
+            )}
+            {!token ? (
+              <>
+                <li>
+                  <NavLink className="btn" to={"/login"}>
+                    Login
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className="btn" to={"/register"}>
+                    Register
+                  </NavLink>
+                </li>
+              </>
+            ) : (
+              <li>
+                <span className="btn" onClick={logoutFunc}>
+                  Logout
+                </span>
+              </li>
+            )}
+          </ul>
+        </nav>
+        <div className="menu-icons">
+          {!iconActive && (
+            <FiMenu
+              className="menu-open"
+              onClick={() => {
+                setIconActive(true);
+              }}
+            />
           )}
-        </ul>
-      </nav>
-      <div className="menu-icons">
-        {!iconActive && (
-          <FiMenu
-            className="menu-open"
-            onClick={() => {
-              setIconActive(true);
-            }}
-          />
-        )}
-        {iconActive && (
-          <RxCross1
-            className="menu-close"
-            onClick={() => {
-              setIconActive(false);
-            }}
-          />
-        )}
-      </div>
-    </header>
+          {iconActive && (
+            <RxCross1
+              className="menu-close"
+              onClick={() => {
+                setIconActive(false);
+              }}
+            />
+          )}
+        </div>
+      </header>
+    </div>
   );
 };
 
